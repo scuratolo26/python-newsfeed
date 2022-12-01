dfc2338b30fc3ffc94f22d6c849df15287bf184b
Samantha Curatolo <85077075+scuratolo26@users.noreply.github.com>
2022-12-01 13:28:51 -0500

    filters

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from app.routes import home, dashboard
 from flask import Flask
 from app.db import init_db
+from app.utils import filters
 
 
 def create_app(test_config=None):
@@ -11,9 +12,9 @@ def create_app(test_config=None):
         SECRET_KEY='super_secret_key'
     )
 
-    @app.route('/hello')
-    def hello():
-        return 'hello world'
+    app.jinja_env.filters['format_url'] = filters.format_url
+    app.jinja_env.filters['format_date'] = filters.format_date
+    app.jinja_env.filters['format_plural'] = filters.format_plural
 
     # register routes
     app.register_blueprint(home)
