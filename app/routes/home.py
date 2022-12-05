4ea1aaaa97f874c202f39d31a119fea7b2bbedd7
Samantha Curatolo <85077075+scuratolo26@users.noreply.github.com>
2022-12-05 12:43:05 -0500

    session

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session, redirect
 from app.models import Post
 from app.db import get_db
 
@@ -13,7 +13,8 @@ def index():
 
     return render_template(
         'homepage.html',
-        posts=posts
+        posts=posts,
+        loggedIn=session.get('loggedIn')
     )
 
 
@@ -31,5 +32,6 @@ def single(id):
     # render single post template
     return render_template(
         'single-post.html',
-        post=post
+        post=post,
+        loggedIn=session.get('loggedIn')
     )
